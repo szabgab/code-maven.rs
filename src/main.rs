bf910b957f97d0278473777037d014fa9b35d969
Gabor Szabo <gabor@szabgab.com>
2023-08-27 10:38:21 +0300

    start writing test case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Page {
+    title: String,
+}
+
+impl Page {
+    pub fn new() -> Page {
+        Page {
+            title: "".to_string(),
+        }
+    }
+}
+
+fn read_md_file(_path: &str) -> Page {
+    Page::new()
+}
+
+
+#[test]
+fn test_read() {
+    let data = read_md_file("examples/pages/index.md");
+    let expected = Page {
+        title: "".to_string(),
+    };
+    assert_eq!(data.title, expected.title);
+}
