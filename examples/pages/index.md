f97e4210107eb4062864706974140d14f869c094
Gabor Szabo <gabor@szabgab.com>
2023-08-27 10:57:46 +0300

    start parsing header

---
+++ examples/pages/index.md
@@ -0,0 +1,5 @@
+=title Index page
+=timestamp 2015-10-11T12:30:01
+
+
+Some Text.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let data = read_md_file("examples/pages/index.md");
+    dbg!(&data);
 }
 
+#[derive(Debug)]
 struct Page {
     title: String,
 }
@@ -14,16 +21,40 @@ impl Page {
     }
 }
 
-fn read_md_file(_path: &str) -> Page {
-    Page::new()
-}
+fn read_md_file(path: &str) -> Page {
+    let mut page = Page::new();
+    match File::open(path) {
+        Ok(file) => {
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                let line = line.unwrap();
+                //dbg!(&line);
+                let re = Regex::new(r"^=([a-z]+) (.*)").unwrap();
+                match re.captures(&line) {
+                    Some(value) => {
+                        //dbg!(&value);
+                        if &value[1] == "title" {
+                            page.title = value[2].to_string();
+                        }
+                    }
+                    None => {}
+                }
+            }
+        }
+        Err(error) => {
+            println!("Error opening file {}: {}", path, error);
+        }
+    }
 
+    page
+}
 
 #[test]
 fn test_read() {
     let data = read_md_file("examples/pages/index.md");
+    dbg!(&data);
     let expected = Page {
-        title: "".to_string(),
+        title: "Index page".to_string(),
     };
     assert_eq!(data.title, expected.title);
 }
