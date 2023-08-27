fbbd0994063eb33ed424d9d103d39f8a12114677
Gabor Szabo <gabor@szabgab.com>
2023-08-27 11:21:04 +0300

    read text as well

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 struct Page {
     title: String,
     timestamp: String,
+    content: String,
 }
 
 impl Page {
@@ -19,6 +20,7 @@ impl Page {
         Page {
             title: "".to_string(),
             timestamp: "".to_string(),
+            content: "".to_string(),
         }
     }
 }
@@ -37,13 +39,16 @@ fn read_md_file(path: &str) -> Page {
                         //dbg!(&value);
                         if &value[1] == "title" {
                             page.title = value[2].to_string();
+                            continue;
                         }
                         if &value[1] == "timestamp" {
                             page.timestamp = value[2].to_string();
+                            continue;
                         }
                     }
                     None => {}
                 }
+                page.content += &line;
             }
         }
         Err(error) => {
@@ -61,7 +66,9 @@ fn test_read() {
     let expected = Page {
         title: "Index page".to_string(),
         timestamp: "2015-10-11T12:30:01".to_string(),
+        content: "Some Text.".to_string(),
     };
     assert_eq!(data.title, expected.title);
     assert_eq!(data.timestamp, expected.timestamp);
+    assert_eq!(data.content, expected.content);
 }
