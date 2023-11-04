99ee11d252f9c84f8da8f3f6618ba0608fabd9c7
Gabor Szabo <gabor@szabgab.com>
2023-11-04 21:52:13 +0200

    start writing sendgreid code

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 set -e
 cargo test
 rm -rf temp/
-cargo run -- --root demo/ --pages demo/pages/ --outdir temp/
+cargo build --release
+./target/release/code-maven --root demo/ --pages demo/pages/ --outdir temp/
 rm -rf temp/img
 echo "---------------------------------"
 diff -r demo_site temp
