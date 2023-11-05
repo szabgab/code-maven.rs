0d9ac971d06c1f7039259e08474b8a96ba97935e
Gabor Szabo <gabor@szabgab.com>
2023-11-05 11:52:49 +0200

    rename executable

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -e
 cargo test
 rm -rf temp/
 cargo build --release
-./target/release/code-maven --root demo/ --pages demo/pages/ --outdir temp/
+./target/release/code-maven-web --root demo/ --pages demo/pages/ --outdir temp/
 rm -rf temp/img
 echo "---------------------------------"
 diff -r demo_site temp
