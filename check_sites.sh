8efe85929353eab1cf22ad643679fc597138424e
Gabor Szabo <gabor@szabgab.com>
2023-11-22 19:04:11 +0200

    remove banner-builder.rs as it needs a /site and bash does not allow it

--- check_sites.sh
+++ check_sites.sh
@@ -1,7 +1,7 @@
 set -e
 
 
-for project in izrael.szabgab.com israel.szabgab.com rust.code-maven.com banner-builder.rs site
+for project in izrael.szabgab.com israel.szabgab.com rust.code-maven.com site
 #for project in site
 do
     if ! test -d "$project"; then
