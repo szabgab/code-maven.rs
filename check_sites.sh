f30e2cf37e31a8d269d6633d466da51269ea59f5
Gabor Szabo <gabor@szabgab.com>
2023-11-22 18:56:19 +0200

    code to check the sites locally and in the CI as well

---
+++ check_sites.sh
@@ -0,0 +1,25 @@
+set -e
+
+
+for project in izrael.szabgab.com israel.szabgab.com rust.code-maven.com banner-builder.rs site
+#for project in site
+do
+    if ! test -d "$project"; then
+        project=../$project
+    fi
+
+    echo $project
+    rm -rf _site/*
+    cargo run --bin code-maven-web -- --root $project  --outdir _site/
+
+    #echo "---------------------------"
+    #for page in _site/*.html
+    #do
+    #    page=$(basename $page)
+    #    echo $page
+    #    page=$(sed "s/html/png/" <<< "$page")
+    #    echo _site/img/$page
+    #    test -f _site/img/$page
+    #done
+
+done
