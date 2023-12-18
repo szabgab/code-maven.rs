911fff05303628e1f5e82e657ad8c012caabc4f9
Gabor Szabo <gabor@szabgab.com>
2023-12-18 08:42:30 +0200

    check projects where the site is in the site folder

--- check_sites.sh
+++ check_sites.sh
@@ -25,6 +25,30 @@ do
 
 done
 
+# projects where the site is in the "site" subfolder.
+for project in site-checker.rs
+do
+    if ! test -d "$project"; then
+        project=../$project
+    fi
+
+    echo "---------------------------"
+    echo $project
+    rm -rf _site/*
+    cargo run --bin code-maven-web -- --root $project/site  --outdir _site/
+
+    echo "---------------------------"
+    for page in _site/*.html
+    do
+        page=$(basename $page)
+        echo $page in $project in $project
+        page=$(sed "s/html/png/" <<< "$page")
+        echo _site/img/$page
+        test -f _site/img/$page
+    done
+
+done
+
 
 echo "-------------------------------"
 echo Finished successfully
