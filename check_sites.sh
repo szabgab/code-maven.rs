ee6ef63ffffc05dddbe7ea18b300a2bb8e365d36
Gabor Szabo <gabor@szabgab.com>
2023-12-18 11:25:46 +0200

    create function to check sites to avoid duplicate code

--- check_sites.sh
+++ check_sites.sh
@@ -1,17 +1,17 @@
 set -e
 
 
-for project in rust.org.il izrael.szabgab.com israel.szabgab.com rust.code-maven.com site
-#for project in site
-do
+function check_project
+{
     if ! test -d "$project"; then
         project=../$project
     fi
 
     echo "---------------------------"
-    echo $project
+    echo "project: $project"
+    echo "folder:  $folder"
     rm -rf _site/*
-    cargo run --bin code-maven-web -- --root $project  --outdir _site/
+    cargo run --bin code-maven-web -- --root $project/$folder  --outdir _site/
 
     echo "---------------------------"
     for page in _site/*.html
@@ -22,31 +22,20 @@ do
         echo _site/img/$page
         test -f _site/img/$page
     done
+}
 
+folder=
+for project in rust.org.il izrael.szabgab.com israel.szabgab.com rust.code-maven.com site
+#for project in rust.org.il site
+do
+    check_project
 done
 
 # projects where the site is in the "site" subfolder.
+folder=site
 for project in site-checker.rs
 do
-    if ! test -d "$project"; then
-        project=../$project
-    fi
-
-    echo "---------------------------"
-    echo $project
-    rm -rf _site/*
-    cargo run --bin code-maven-web -- --root $project/site  --outdir _site/
-
-    echo "---------------------------"
-    for page in _site/*.html
-    do
-        page=$(basename $page)
-        echo $page in $project in $project
-        page=$(sed "s/html/png/" <<< "$page")
-        echo _site/img/$page
-        test -f _site/img/$page
-    done
-
+    check_project
 done
 
 
