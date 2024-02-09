f745816860a2304c8f3d132977b48e53bd286455
Gabor Szabo <gabor@szabgab.com>
2024-02-09 11:26:56 +0200

    test generating site using skeleton

--- tests/test_new.rs
+++ tests/test_new.rs
@@ -15,6 +15,17 @@ fn new_site(root: &str) {
     assert_eq!(result.status, ExitStatus::from_raw(0));
 }
 
+fn generate_site(root: &str, outdir: &str) {
+    let result = Command::new("cargo")
+        .args(["run", "-q", "web", "--root", root, "--outdir", outdir])
+        .output()
+        .expect("command failed to start");
+    //canonicalize()
+    assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "");
+    assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
+    assert_eq!(result.status, ExitStatus::from_raw(0));
+}
+
 #[test]
 fn test_new_without_parameters() {
     // TODO create external test that will create a new blog using the "new" command
@@ -77,4 +88,34 @@ fn test_new_with_parameters() {
         .collect::<Vec<String>>();
     content.sort();
     assert_eq!(content, ["about.md", "index.md"])
+}
+
+#[test]
+fn test_new_generate() {
+    let tmp_dir = TempDir::new("example").unwrap();
+    println!("tempdir: {:?}", tmp_dir);
+
+    new_site(tmp_dir.path().join("site").to_str().unwrap());
+
+    let outdir = tmp_dir.path().join("out");
+    assert!(!outdir.exists());
+    generate_site(
+        tmp_dir.path().join("site").to_str().unwrap(),
+        outdir.to_str().unwrap(),
+    );
+    assert!(outdir.exists());
+    assert!(outdir.join("index.html").exists());
+    assert!(outdir.join("robots.txt").exists());
+    assert!(outdir.join("sitemap.xml").exists());
+    assert!(outdir.join("atom").exists());
+    assert!(outdir.join("archive.html").exists());
+    assert!(outdir.join("about.html").exists());
+
+    assert!(outdir.join("tags").join("about.html").exists());
+    assert!(outdir.join("tags").join("blog.html").exists());
+    assert!(outdir.join("tags").join("index.html").exists());
+
+    assert!(outdir.join("img").join("about.png").exists());
+    assert!(outdir.join("img").join("archive.png").exists());
+    assert!(outdir.join("img").join("index.png").exists());
 }
