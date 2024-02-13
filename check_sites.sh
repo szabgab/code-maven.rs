a5fa5943da63a7e37a2b766323106b465f3a65d7
Gabor Szabo <gabor@szabgab.com>
2024-02-13 11:26:41 +0200

    move the cloning of repos into the check_sites script

--- check_sites.sh
+++ check_sites.sh
@@ -3,15 +3,10 @@ set -e
 
 function check_project
 {
-    if ! test -d "$project"; then
-        project=../$project
-    fi
-
     echo "---------------------------"
-    echo "project: $project"
-    echo "folder:  $folder"
-    rm -rf _site/*
-    cargo run --bin code-maven -- web --root $project/$folder  --outdir _site/
+    echo "site:    $site"
+    rm -rf _site/
+    cargo run --bin code-maven -- web --root $site  --outdir _site/
 
     echo "---------------------------"
     for page in _site/*.html
@@ -24,19 +19,78 @@ function check_project
     done
 }
 
-folder=
-for project in rust.org.il izrael.szabgab.com israel.szabgab.com rust.code-maven.com site
-#for project in rust.org.il site
-do
-    check_project
-done
+function check_public_projects
+{
+    for url in \
+        https://github.com/szabgab/rust.code-maven.com \
+        https://github.com/szabgab/rust.org.il \
+        https://github.com/szabgab/banner-builder.rs \
+        https://github.com/szabgab/site-checker.rs \
+        https://github.com/moshe742/my_blog
+    do
+        echo "Processing $url"
+        cd $root
+        folder=${url##*/}
+        echo "folder: $folder"
+        if [ -e sites/$folder ]
+        then
+            echo "exists. git pull"
+            cd sites/$folder
+            git pull
+        else
+            echo "git clone"
+            cd sites
+            git clone $url
+            cd $folder
+        fi
+        cd $root
+        site=sites/$folder
 
-# projects where the site is in the "site" subfolder.
-folder=site
-for project in site-checker.rs
-do
-    check_project
-done
+        if [ "$folder" == "banner-builder.rs" ] || [ "$folder" == "site-checker.rs" ]
+        then
+            echo "site"
+            site=$site/site
+        fi
+
+        check_project
+    done
+}
+
+
+
+function check_private_projects
+{
+    for project in izrael.szabgab.com israel.szabgab.com
+    do
+        echo "private project $project"
+        if [ -e ../$project ];
+        then
+            site=../$project
+        else
+            if [ -e sites/$project ];
+            then
+                site=sites/$project
+            else
+                echo skipping private site
+                continue
+            fi
+        fi
+
+        check_project
+    done
+}
+
+
+root=$(pwd)
+echo "root=$root"
+mkdir -p sites
+
+site=site
+check_project
+
+check_public_projects
+check_private_projects
+
 
 
 echo "-------------------------------"
