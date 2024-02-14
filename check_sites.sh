52e96d8a7a1772ce27a40a3f7ab34bcd64666882
Gabor Szabo <gabor@szabgab.com>
2024-02-14 13:04:59 +0200

    temporarily disable site checking till the fix is accepted

--- check_sites.sh
+++ check_sites.sh
@@ -19,14 +19,14 @@ function check_project
     done
 }
 
+        # https://github.com/moshe742/my_blog \
 function check_public_projects
 {
     for url in \
         https://github.com/szabgab/rust.code-maven.com \
         https://github.com/szabgab/rust.org.il \
         https://github.com/szabgab/banner-builder.rs \
-        https://github.com/szabgab/site-checker.rs \
-        https://github.com/moshe742/my_blog
+        https://github.com/szabgab/site-checker.rs
     do
         echo "Processing $url"
         cd $root
