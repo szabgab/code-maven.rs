80f72c42b377fe4f8097d4fe4ea09fb552927439
Gabor Szabo <gabor@szabgab.com>
2024-02-14 09:31:45 +0200

    test the drafts command

---
+++ tests/test_drafts.rs
@@ -0,0 +1,34 @@
+use std::{
+    os::unix::process::ExitStatusExt,
+    process::{Command, ExitStatus},
+};
+
+#[test]
+fn test_drafts_demo() {
+    let result = Command::new("cargo")
+        .args(["run", "-q", "drafts", "--root", "test_cases/demo"])
+        .output()
+        .expect("command failed to start");
+    //canonicalize()
+    assert_eq!(
+        std::str::from_utf8(&result.stdout).unwrap(),
+        "\n---- Drafts ----\ndraft.md                       Draft page\n"
+    );
+    assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
+    assert_eq!(result.status, ExitStatus::from_raw(0));
+}
+
+#[test]
+fn test_drafts_site() {
+    let result = Command::new("cargo")
+        .args(["run", "-q", "drafts", "--root", "site"])
+        .output()
+        .expect("command failed to start");
+    //canonicalize()
+    assert_eq!(
+        std::str::from_utf8(&result.stdout).unwrap(),
+        "\n---- Drafts ----\n"
+    );
+    assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
+    assert_eq!(result.status, ExitStatus::from_raw(0));
+}
