94b3bc54676ebc5c62dcdf272d47c46f10f371ac
Gabor Szabo <gabor@szabgab.com>
2024-02-14 08:43:33 +0200

    convert the test.sh into functions

--- test.sh
+++ test.sh
@@ -1,8 +1,45 @@
 set -e
-cargo test
-rm -rf temp/
-cargo build --release
-./target/release/code-maven web --root test_cases/demo/ --outdir temp/
-rm -rf temp/img
-echo "---------------------------------"
-diff -r generated/demo temp
+
+function run_tests() {
+    cargo test
+}
+
+function generate() {
+    rm -rf temp/
+    cargo build --release
+    ./target/release/code-maven web --root $source --outdir temp/
+    rm -rf temp/img
+    echo "---------------------------------"
+    diff -r generated/$param temp
+}
+
+#if [ "$*" == "" ]
+#then
+#    echo "Missing parameters from the command line"
+#    exit 1
+#fi
+
+if [ "$*" == "" ]
+then
+    params="test demo"
+else
+    params=$@
+fi
+
+#echo "params: $params"
+
+for param in $params
+do
+    #echo "param: $param"
+
+    if [ $param == "test" ]
+    then
+        run_tests
+        continue
+    fi
+
+    source=test_cases/$param/
+    generate
+done
+
+
